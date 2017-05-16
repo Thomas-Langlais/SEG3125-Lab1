e64aedf644fdba24e01a679f7c4ef8b218a2a55a
Thomas L'Anglais <tlang072@uottawa.ca>
2017-05-16 11:05:04 -0400

    first commit

---
+++ app/src/main/java/lab1/tlang072/tipcalculator/activities/CalculatorActivity.java
@@ -0,0 +1,15 @@
+package lab1.tlang072.tipcalculator.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lab1.tlang072.tipcalculator.R;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
